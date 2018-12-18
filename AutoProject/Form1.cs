f401081c7f287c1292e3020ce00f392b25987918
eboxmaker <995207301@qq.com>
2018-12-18 20:00:51 +0800

    first commit

---
+++ AutoProject/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace AutoProject
+{
+
+    public partial class Form1 : Form
+    {
+        excleOpt excle = new excleOpt();
+        DataTable dt = new DataTable();
+        Target mcu = new Target();
+        jsonHlep js = new jsonHlep();
+        JsonManager jm = new JsonManager();
+
+        JObject jo = new JObject();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string str = System.AppDomain.CurrentDomain.BaseDirectory;
+            FileInfo file = new FileInfo("McuConfiglist.xlsx");
+            string FileFullPath = file.FullName.ToString();
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+
+            jo = JsonManager.OpenFile(".\\mcu\\mcusFeaturesAndDescription.json");
+            string jstr = JsonManager.Serialize(jo);
+
+            JsonManager.Save(jo, ".\\mcu\\mcusFeaturesAndDescription1.json");
+
+            dt = JsonManager.toDataTable(jstr);
+            dataGridView1.DataSource = dt;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                comboBox1.Items.Add(dt.Rows[i][0]);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnUpdataConfigHeader_Click(object sender, EventArgs e)
+        {
+            mcu.updataMcuConfigHeader();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = comboBox1.SelectedItem.ToString();
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (comboBox1.SelectedItem.ToString() == dt.Rows[i][0].ToString())
+                {
+                    //int j = 0;
+                    //mcu.name = dt.Rows[i][j++].ToString();
+                    //mcu.Package = dt.Rows[i][j++].ToString();
+                    //mcu.FlashSzie = Convert.ToInt16(dt.Rows[i][j++]);
+                    //mcu.RAMSize = Convert.ToInt16(dt.Rows[i][j++]);
+                    //mcu.IoNum = Convert.ToInt16(dt.Rows[i][j++]);
+                    //mcu.Freq = Convert.ToInt16(dt.Rows[i][j++]);
+                    //mcu.PinNum = mcu.getPinNum();
+                    //mcu.FatherType = mcu.parserFatherType();
+
+                    //j = 1;
+                    //listBox2.Items.Clear();
+                    //for (int k = 1; k < dt.Columns.Count; k++)
+                    //{
+                    //    listBox2.Items.Add(dt.Rows[i][j++].ToString());
+                    //}
+                    string strFilter = String.Format("RPN like '%{0}%'", textBox1.Text);  //我这里是用的数据库 Project字段来筛选的，可以根据需要自己定义。
+                    DataView dv = dt.DefaultView;
+                    dv.RowFilter = strFilter;
+
+                    dataGridView1.DataSource = dv;
+
+                }
+            }
+
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            string str = comboBox1.Text;
+            string strFilter = String.Format("RPN like '%{0}%'", str);  //我这里是用的数据库 Project字段来筛选的，可以根据需要自己定义。
+            DataView dv = dt.DefaultView;
+            dv.RowFilter = strFilter;
+
+            dataGridView1.DataSource = dv;
+        }
+
+
+
+    }
+}
---
+++ AutoProject/JsonManager.cs
@@ -0,0 +1,302 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProject
+{
+    class JsonManager
+    {
+       // public JObject jo;
+
+        public static JObject OpenFile(string path)
+        {
+            JObject obj;
+            FileStream fs = File.OpenRead(path);
+            if (fs == null)
+                obj = null;
+            else
+            {
+                int len = (int)fs.Length;
+                byte[] byteArray = new byte[len];
+                int ReadedLength = fs.Read(byteArray, 0, len);
+                string js = Encoding.Default.GetString(byteArray);
+                obj = (JObject)JsonConvert.DeserializeObject(js);
+            }
+            return obj;
+        }
+        //public static string OpenFile(string path)
+        //{
+        //    string jstr;
+        //    FileStream fs = File.OpenRead(path);
+        //    if (fs == null)
+        //        jstr = null;
+        //    else
+        //    {
+        //        int len = (int)fs.Length;
+        //        byte[] byteArray = new byte[len];
+        //        int ReadedLength = fs.Read(byteArray, 0, len);
+        //        jstr = Encoding.Default.GetString(byteArray);
+        //    }
+        //    return jstr;
+        //}
+        
+        public DataTable JobjectToDataTable(JObject obj)
+        {
+            DataTable dt = new DataTable();
+            return dt;
+        }
+        public static string Serialize(string str)
+        {
+            //格式化json字符串
+            JsonSerializer serializer = new JsonSerializer();
+            TextReader tr = new StringReader(str);
+            JsonTextReader jtr = new JsonTextReader(tr);
+            object obj = serializer.Deserialize(jtr);
+            if (obj != null)
+            {
+                StringWriter textWriter = new StringWriter();
+                JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
+                {
+                    Formatting = Formatting.Indented,
+                    Indentation = 4,
+                    IndentChar = ' '
+                };
+                serializer.Serialize(jsonWriter, obj);
+                return textWriter.ToString();
+            }
+            else
+            {
+                return str;
+            }
+        }
+        public static string Serialize(JObject obj)
+        {
+            //格式化json字符串
+            JsonSerializer serializer = new JsonSerializer();
+
+            StringWriter textWriter = new StringWriter();
+            JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
+            {
+                Formatting = Formatting.Indented,
+                Indentation = 4,
+                IndentChar = ' '
+            };
+            serializer.Serialize(jsonWriter, obj);
+            return textWriter.ToString();
+        }
+        private static byte[] GetByte(JObject obj)
+        {
+            string js = Serialize(obj);
+            byte[] bytes = Encoding.Default.GetBytes(js);
+            return bytes;
+        }
+        public static bool Save(JObject obj, string filepath)
+        {
+            FileStream fs = File.Create(filepath);
+            byte[] byteArray = GetByte(obj);
+            fs.Write(byteArray, 0, (int)byteArray.Length);
+            fs.Flush();
+            fs.Close();
+            return true;
+        }
+        public static bool Save(string jstr, string filepath)
+        {
+            FileStream fs = File.Create(filepath);
+            byte[] byteArray = Encoding.Default.GetBytes(jstr);
+            fs.Write(byteArray, 0, (int)byteArray.Length);
+            fs.Flush();
+            fs.Close();
+            return true;
+        }
+
+        public static DataTable toDataTable(string jstring)
+        {
+            DataTable dt=new DataTable();
+            JArray ay = new JArray();
+            JObject json = JObject.Parse(jstring);
+
+            dt.Columns.Add("RPN",typeof(string));
+            dt.Columns.Add("refname", typeof(string));
+            dt.Columns.Add("package", typeof(string));
+            dt.Columns.Add("flash", typeof(string));
+            dt.Columns.Add("ram", typeof(string));
+            dt.Columns.Add("iONb", typeof(string));
+            dt.Columns.Add("frequency", typeof(string));
+            ay = JArray.Parse(json["MCUs"].ToString());
+            for (int i = 0; i < ay.Count;i++ )
+            {
+                JArray aay = JArray.Parse(ay[i]["subpackages"].ToString());
+                for(int j = 0 ; j < aay.Count;j++)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr[0] = ay[i]["RPN"].ToString();
+                    dr[1] = aay[j]["refname"].ToString();
+                    dr[2] = aay[j]["package"].ToString();
+
+                    dr[3] = ay[i]["flash"].ToString();
+                    dr[4] = ay[i]["ram"].ToString();
+                    dr[5] = aay[j]["iONb"].ToString();
+                    dr[6] = ay[i]["frequency"].ToString();
+                    dt.Rows.Add(dr);
+
+                }
+            }
+            return dt;
+
+        }
+
+        //public DataTable todt(string str)
+        //{
+        //    JArray listy = JArray.Parse(str);
+
+        //    if (listy != null && listy.Count > 0)
+        //    {
+
+        //        foreach (JToken jt in listy)
+        //        {
+
+        //            foreach (IJEnumerable<JToken> jp in jt.Values())
+        //            {
+                        
+        //                JsonArrayObjects QW = JObject.Parse(jt.ToString());
+
+        //                for (int j = 0; j < QW.Count; j++)
+        //                {
+
+        //                    foreach (var qwqw in QW[j])
+        //                    {
+
+        //                        Logger.Info(qwqw.Key + "------" + qwqw.Value);
+
+        //                    }
+
+        //                }
+
+        //            }
+
+        //        }
+
+        //    }
+        //}
+        /// <summary>
+        /// Json 字符串 转换为 DataTable数据集合
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        //public static DataTable ToDataTable(string json)
+        //{
+        //    DataTable dataTable = new DataTable();  //实例化
+        //    DataTable result;
+        //    try
+            //{
+                //JsonSerializer javaScriptSerializer = new JsonSerializer();
+                //JsonReader reader = new JsonReader(json);
+                //JsonSerializer.MaxJsonLength = Int32.MaxValue; //取得最大数值
+               // ArrayList arrayList = JsonSerializer
+            //    if (arrayList.Count > 0)
+            //    {
+            //        foreach (Dictionary<string, object> dictionary in arrayList)
+            //        {
+            //            if (dictionary.Keys.Count<string>() == 0)
+            //            {
+            //                result = dataTable;
+            //                return result;
+            //            }
+            //            //Columns
+            //            if (dataTable.Columns.Count == 0)
+            //            {
+            //                foreach (string current in dictionary.Keys)
+            //                {
+            //                    if (current != "data")
+            //                        dataTable.Columns.Add(current, dictionary[current].GetType());
+            //                    else
+            //                    {
+            //                        ArrayList list = dictionary[current] as ArrayList;
+            //                        foreach (Dictionary<string, object> dic in list)
+            //                        {
+            //                            foreach (string key in dic.Keys)
+            //                            {
+            //                                dataTable.Columns.Add(key, dic[key].GetType());
+            //                            }
+            //                            break;
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //            //Rows
+            //            string root = "";
+            //            foreach (string current in dictionary.Keys)
+            //            {
+            //                if (current != "data")
+            //                    root = current;
+            //                else
+            //                {
+            //                    ArrayList list = dictionary[current] as ArrayList;
+            //                    foreach (Dictionary<string, object> dic in list)
+            //                    {
+            //                        DataRow dataRow = dataTable.NewRow();
+            //                        dataRow[root] = dictionary[root];
+            //                        foreach (string key in dic.Keys)
+            //                        {
+            //                            dataRow[key] = dic[key];
+            //                        }
+            //                        dataTable.Rows.Add(dataRow);
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            //catch
+            //{
+            //}
+            //result = dataTable;
+            //return result;
+        //}
+
+
+        /// <summary> 
+        /// Datatable转换为Json 
+        /// </summary> 
+        /// <param name="table">Datatable对象</param> 
+        /// <returns>Json字符串</returns> 
+        public static string ToJson(DataTable dt)
+        {
+            StringBuilder jsonString = new StringBuilder();
+            jsonString.Append("[");
+            DataRowCollection drc = dt.Rows;
+            for (int i = 0; i < drc.Count; i++)
+            {
+                jsonString.Append("{");
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    string strKey = dt.Columns[j].ColumnName;
+                    string strValue = drc[i][j].ToString();
+                    Type type = dt.Columns[j].DataType;
+                    jsonString.Append("\"" + strKey + "\":");
+                  //  strValue = StringFormat(strValue, type);
+                    if (j < dt.Columns.Count - 1)
+                    {
+                        jsonString.Append(strValue + ",");
+                    }
+                    else
+                    {
+                        jsonString.Append(strValue);
+                    }
+                }
+                jsonString.Append("},");
+            }
+            jsonString.Remove(jsonString.Length - 1, 1);
+            jsonString.Append("]");
+            return jsonString.ToString();
+        }
+
+    }
+}
